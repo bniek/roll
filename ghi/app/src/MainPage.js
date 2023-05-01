577e772206fa6e53eec9e69726f59565304118a6
bennie <manabinbe@gmail.com>
2023-05-01 18:36:57 -0500

    styling

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,23 +1,26 @@
 function MainPage() {
   return (
-    <div className="px-4 py-5 my-5 text-center">
+    <div className="text-center px-4 py-3 my-5">
       <h1 style={{ color: 'white', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '1', fontSize: '7rem'}} className="display-5 fw-bold">ROLL</h1>
       <div style={{ position: 'absolute', top: '60%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '1' }} className="col-lg-6 mx-auto">
       </div>
       <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
         <div className="carousel-item active" style={{ height: "80vh" }}>
+            <img src="https://images.pexels.com/photos/1082653/pexels-photo-1082653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="side of dark green car parked infront of greenery" />
+        </div>
+        <div className="carousel-item" style={{ height: "80vh" }}>
           <a href= "http://localhost:3000/automobiles" >
-            <img src="https://images.pexels.com/photos/2739286/pexels-photo-2739286.jpeg?auto=compress&cs=tinysrgb&w=800" className="d-block w-100" alt="pink vintage car with hood open and headlights" />
+            <img src="https://images.pexels.com/photos/253096/pexels-photo-253096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="pink vintage car with hood open and headlights" />
           </a>
-          <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', color: 'white', padding: '10px' }}>
+          <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)', color: 'white', padding: '10px' }}>
             <h1 className="carousel_header">INVENTORY</h1>
             <p>Keep track of your dealership's inventory</p>
         </div>
         </div>
         <div className="carousel-item" style={{ height: "80vh" }}>
           <a href = "http://localhost:3000/appointments">
-            <img src="https://images.pexels.com/photos/1768826/pexels-photo-1768826.jpeg?auto=compress&cs=tinysrgb&w=800" className="d-block w-100" alt="side of dark green car parked infront of greenery" />
+            <img src="https://images.pexels.com/photos/1573424/pexels-photo-1573424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="side of dark green car parked infront of greenery" />
           </a>
           <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)', color: 'white', padding: '10px' }}>
             <h1 className="carousel_header">SERVICE</h1>
@@ -26,9 +29,9 @@ function MainPage() {
         </div>
         <div className="carousel-item" style={{ height: "80vh" }}>
           <a href = "http://localhost:3000/sales/">
-            <img src="https://images.pexels.com/photos/1974520/pexels-photo-1974520.jpeg?auto=compress&cs=tinysrgb&w=1600" className="d-block w-100" alt="whit old fashioned cadillac with palm trees in the background" />
+            <img src="https://images.pexels.com/photos/210156/pexels-photo-210156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className="d-block w-100" alt="whit old fashioned cadillac with palm trees in the background" />
           </a>
-          <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.1)', color: 'white', padding: '10px' }}>
+          <div className="carousel-caption d-none d-md-block" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)', color: 'white', padding: '10px' }}>
           <h1 className="carousel_header">SALES</h1>
           <p>Manage all sales</p>
         </div>
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,18 +9,6 @@ function Nav() {
           <div className="d-flex justify-content-end">
             <div className="nav-item dropdown">
               <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                Sales
-              </button>
-              <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/new">New Customers</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/new">New Sales Representative</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/new">New Sales Records</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/sales">All Sales Records</NavLink></li>
-                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/history">Sales By Employee</NavLink></li>
-              </div>
-            </div>
-            <div className="nav-item dropdown">
-              <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
                 Inventory
               </button>
               <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
@@ -30,6 +18,18 @@ function Nav() {
                 <li><NavLink className="dropdown-item" aria-current="page" to="models/">All Models</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="automobiles/new/">New Automobile</NavLink></li>
                 <li><NavLink className="dropdown-item" aria-current="page" to="automobiles/">All Automobiles</NavLink></li>
+              </div>
+            </div>
+            <div className="nav-item dropdown">
+              <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                Sales
+              </button>
+              <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/new">New Customers</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/new">New Sales Representative</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/new">New Sales Records</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales">All Sales Records</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/history">Sales By Employee</NavLink></li>
               </div>
             </div>
             <div className="nav-item dropdown" style={{ paddingRight: '10px' }}>
