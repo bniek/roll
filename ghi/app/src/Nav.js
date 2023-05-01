788b7d82f0bc2787aeefae0866e6e64dfd0793db
bennie <manabinbe@gmail.com>
2023-05-01 18:21:02 -0500

    nav bar updates

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,58 +2,54 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/manufacturers">Manufacturers</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/models">Models</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/automobiles">Automobiles</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/customers/new">Add a potential customer</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/sales">All sales</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/salespeople/history">Sales Person History</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/salespeople/new">Add a sales person</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/sales/new">Create a sales record</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/technicians/new">Add technician</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/appointments">Appointments</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/appointments/new">Make an appointment</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/appointments/history">Appointment History</NavLink>
-          </li>
-          </ul>
+    <>
+      <nav className="navbar" style={{backgroundColor: '#223835'}}>
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/" style={{ color: '#ffffff', paddingLeft: '20px' }}>ROLL</NavLink>
+          <div className="d-flex justify-content-end">
+            <div className="nav-item dropdown">
+              <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                Sales
+              </button>
+              <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                <li><NavLink className="dropdown-item" aria-current="page" to="/customers/new">New Customers</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/new">New Sales Representative</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales/new">New Sales Records</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/sales">All Sales Records</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/salespeople/history">Sales By Employee</NavLink></li>
+              </div>
+            </div>
+            <div className="nav-item dropdown">
+              <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                Inventory
+              </button>
+              <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers/new/">New Manufacturer</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="manufacturers/">All Manufacturers</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="models/new/">New Vehicle Model</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="models/">All Models</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="automobiles/new/">New Automobile</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="automobiles/">All Automobiles</NavLink></li>
+              </div>
+            </div>
+            <div className="nav-item dropdown" style={{ paddingRight: '10px' }}>
+              <button style={{borderColor: '#223835', backgroundColor: '#223835'}} className="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                Service
+              </button>
+              <div className="dropdown-menu dropdown-menu-end dropdown-menu-dark open" aria-labelledby="dropdownMenuButton2">
+                <li><NavLink className="dropdown-item" aria-current="page" to="/technicians/new">Create a Technician</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/technicians">All Technician</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/new">Create Service</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/appointments/history">Service History</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="/appointments">Service Appointments</NavLink></li>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    </>
+  );
+
 }
 
 export default Nav;
